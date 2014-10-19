3227feb7b88128be20227cf23890bd9dba4e937b
Felix Kaiser <felix.kaiser@fxkr.net>
2014-10-20 01:00:21 +0200

    Add recaptcha support

--- freeoids/views.py
+++ freeoids/views.py
@@ -1,12 +1,22 @@
 import flask
+import flask_wtf
 
 from freeoids import app
 from freeoids import backend
 
 
+class RequestForm(flask_wtf.Form):
+    recaptcha = flask_wtf.RecaptchaField()
+
+
 @app.route("/")
 def index():
-    return flask.render_template('index.html')
+    form = RequestForm()
+    use_recaptcha = app.config.get('RECAPTCHA_ENABLED')
+    if not use_recaptcha:
+        del form.recaptcha
+
+    return flask.render_template('index.html', form=form, use_recaptcha=use_recaptcha)
 
 @app.route("/assignment", methods=["GET"])
 def assignment_get():
@@ -14,6 +24,13 @@ def assignment_get():
 
 @app.route("/assignment", methods=["POST"])
 def assignment():
+    form = RequestForm()
+    use_recaptcha = app.config.get('RECAPTCHA_ENABLED')
+    if not use_recaptcha:
+        del form.recaptcha
+
     new_assignment = backend.assign_oid()
+    if not form.validate_on_submit():
+        return flask.redirect(flask.url_for('index'))
     return flask.render_template('assignment.html', assignment=new_assignment)
 
